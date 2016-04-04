a15523c1aaa77069db3add715b22ad626709f514
Daren Schaad and Taylor Pokoj <pokojt@gmail.com>
2016-04-04 13:52:50 -0700

    Add listings to categories

--- app/components/new-listing.js
+++ app/components/new-listing.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import moment from 'moment';
 
 export default Ember.Component.extend({
   addNewListing: false,
@@ -7,15 +8,18 @@ export default Ember.Component.extend({
       this.set('addNewListing', true);
     },
     saveListing() {
+      var date = new Date();
+      date = date.toString().substring(4, 15);
+      var momentDateAndTime = moment(date).format('MM/DD/YYYY');
       var params = {
-        title: this.get('title'),
-        author: this.get('author'),
-        price: this.get('price'),
-        description: this.get('description'),
+        title: this.get('title') ? this.get('title'):"",
+        author: this.get('author') ? this.get('author'):"",
+        price: this.get('price') ? this.get('price'):"",
+        description: this.get('description') ? this.get('description'):"",
         category: this.get('category'),
-        image: this.get('image'),
-        date: this.get('date'),
-        location: this.get('location'),
+        image: this.get('image') ? this.get('image'):"",
+        date: momentDateAndTime,
+        location: this.get('location') ? this.get('location'):"",
       };
       this.set('addNewListing', false);
       this.sendAction('saveListing', params);
